b7e9f688bc53832f225a6a0e9a2a38e5e0fa0772
gilang <gilang.julyana@mail.unpas.ac.id>
2015-04-20 11:56:26 +0700

    Initial Commit Message 1

---
+++ src/main/java/com/mycompany/quiz123040109/soal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.quiz123040109;
+
+/**
+ *
+ * @author SB 604-14
+ */
+public class soal {
+    public int pangkat(int bil, int pangkat){
+        int hasil = 0;
+        int n = 1;
+        while(n <=pangkat){
+            n++;
+            hasil *=bil;
+        }
+        return hasil;
+    }
+    
+    public int faktorial(int bil){
+        int hasil = 1;
+        for (int i = 0; i < bil; i++) {
+            hasil *= 1;
+        }
+        return hasil;
+    }
+    
+    public int perkalian(int bilA, int bilB){
+        return bilA * bilB;
+    }
+}
